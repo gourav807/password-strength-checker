83dddb7cf57ccc977933bbdcd87a1d36d4118c7f
gourav807 <gouravdeep30096@gmail.com>
2025-04-19 12:56:04 +0530

    Update test_password_strength.py

--- tests/test_password_strength.py
+++ tests/test_password_strength.py
@@ -7,13 +7,13 @@ from password_strength_checker import evaluate_password_strength
 
 class TestPasswordStrength(unittest.TestCase):
 
-    def test_weak_password(self):
+    def test_01_weak_password(self):
         self.assertEqual(evaluate_password_strength("12345"), "Weak")
 
-    def test_moderate_password(self):
+    def test_02_moderate_password(self):
         self.assertEqual(evaluate_password_strength("abc123ABC"), "Moderate")
 
-    def test_strong_password(self):
+    def test_03_strong_password(self):
         self.assertEqual(evaluate_password_strength("Str0ng@Pass"), "Strong") 
 
 if __name__ == "__main__":
